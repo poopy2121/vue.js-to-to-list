a2864fa27dadd0c9c11892c5aba5c950e99058a2
poopy2121 <ggg1111g1g1g@gmail.com>
2024-10-28 11:50:55 +0100

    component erstellt und css in app.vue gepackt

---
+++ vue to do list/src/AddButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  inputContent: String,
+  inputCategory: [String, null],
+});
+
+const emit = defineEmits(["add-todo"]);
+
+const isDisabled = computed(() => !props.inputContent.trim() || !props.inputCategory);
+
+const handleClick = () => {
+  if (!isDisabled.value) {
+    emit("add-todo");
+  }
+};
+</script>
+
+<template>
+  <input
+    type="submit"
+    value="Add todo"
+    :disabled="isDisabled"
+    @click="handleClick"
+  />
+</template>
